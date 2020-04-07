4be815dda9b36a5cb59c8c61c76aae5074426f48
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-07 11:37:26 -0500

    -project now in Git

---
+++ Sportsball Overthrown/Assets/instantiateBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateBall : MonoBehaviour
+{
+
+    public GameObject dodgeballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(dodgeballPrefab, gameObject.transform);
+    }
+}
