d11508dbdf29672fe39c7333d68cc6eaf8fddfc0
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-13 16:17:58 -0500

    -ragdoll physics

---
+++ Sportsball - Overthrown/Assets/RagdollCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollCamera : MonoBehaviour
+{
+
+    public GameObject ragdoll;
+
+    private Vector3 offset;
+
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = gameObject.transform.position - ragdoll.transform.position;
+        
+    }
+
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ragdoll.GetComponent<Rigidbody>().AddForce(new Vector3 (.5f,1,0) * 600, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ragdoll.transform.position + offset;
+    }
+}
