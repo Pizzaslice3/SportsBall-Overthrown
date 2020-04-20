d12dfde2aef79379ed5a38b83d24226644af2567
Pizzaslice3 <lcpiazza626@gmail.com>
2020-04-20 18:59:00 -0500

    -enemies now fall when hit

---
+++ Sportsball - Overthrown/Assets/EnemyDie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDie : MonoBehaviour
+{
+
+    public CapsuleCollider topHalf;
+    public CapsuleCollider bottomHalf;
+    public Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            Rigidbody rb = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+
+            _rb = rb;
+
+            if(_rb != null)
+            _rb.AddForce(Vector3.back * 20, ForceMode.Impulse);
+        }
+    }
+}
